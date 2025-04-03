50f1207efe2f93c55638161f6bfbe275ea111e07
Arantis_jr <arantisjunior@gmail.com>
2025-04-03 02:56:55 +0100

    Add: started question 5 and finshed

---
+++ Q5/05xfor_to while.py
@@ -0,0 +1,11 @@
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisú', 'Chocolate Cake']
+i = 0
+list_length = len(favorites)
+
+while i < list_length:
+    print('One of my favorite desserts is', favorites[i])
+    i = i + 1
+    
+
+# for dessert in favorites:
+#     print('One of my favorite desserts is', dessert)
